44762f1b86f1795953fe4f6dbee82c566b2c0918
LanLi2017 <lilan.scut@gmail.com>
2024-06-10 16:55:08 -0500

    5 data cleaning cases [consecutive]

--- history_update_problem/pipeline.py
+++ history_update_problem/pipeline.py
@@ -2,13 +2,14 @@ import pandas as pd
 
 
 def main():
-    data_input = "data_input/menu.csv"
+    parent_folder = "../pd_exp_prep"
+    data_input = f"{parent_folder}/data_input/menu.csv"
     input_df = pd.read_csv(data_input)
     # remove NA
     prep_df = input_df.dropna(subset=['physical_description'])
     # Sample 1000 rows
     df = prep_df.sample(n=100, random_state=42)
-    df.to_csv()
+    df.to_csv("sample_data/menu_sp.csv", index=False)
 
 
 if __name__ == '__main__':
